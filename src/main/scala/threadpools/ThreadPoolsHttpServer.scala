de9fb09e14428541a2f9843084434d543b0e8fd2
Sébastian Le Merdy <sebastian.lemerdy@compte-nickel.fr>
2018-06-27 04:35:20 +0200

    Mutualizes measure into one function

--- src/main/scala/threadpools/ThreadPoolsHttpServer.scala
+++ src/main/scala/threadpools/ThreadPoolsHttpServer.scala
@@ -7,51 +7,48 @@ import java.util.logging.Logger
 import chrono.ChronoManager
 import com.sun.net.httpserver.{HttpExchange, HttpServer}
 
-import scala.concurrent.{ExecutionContextExecutorService, Future}
+import scala.concurrent.{ExecutionContext, Future}
 import scala.concurrent.JavaConversions._
-import scala.util.Success
 
 object ThreadPoolsHttpServer extends App {
-  System.setProperty("java.util.logging.config.file",
-                     "src/main/resources/logging.properties")
+
+  System.setProperty("java.util.logging.config.file", "src/main/resources/logging.properties")
   val log: Logger = Logger.getLogger("threadpools")
   val backlog: Int = 0
-  val server: HttpServer =
-    HttpServer.create(new InetSocketAddress(8080), backlog)
+  val server: HttpServer = HttpServer.create(new InetSocketAddress(8080), backlog)
   val chronoManager: ChronoManager = new ChronoManager()
   var nonBlockingIOPolling: ExecutorService = Executors.newSingleThreadExecutor()
   server.setExecutor(nonBlockingIOPolling)
-  val blockingIOThreadPool: ExecutionContextExecutorService = Executors.newCachedThreadPool()
+  val blockingIOThreadPool: ExecutionContext = Executors.newCachedThreadPool()
   server.createContext(
-    "/threadpools", { exchange: HttpExchange =>
-      chronoManager.start()
-
-      val blockingIOResult = Future {
-        chronoManager.start()
-        blockIO()
-        chronoManager.stop()
-      }(blockingIOThreadPool)
-
-      blockingIOResult.andThen {
-        case Success(_) =>
-          chronoManager.start()
-          exchange.sendResponseHeaders(200, 0)
-          exchange.close()
-
-          chronoManager.stop()
+    "/threadpools", { exchange: HttpExchange ⇒
+      measure {
+        Future {
+          measure {
+            blockIO()
+          }
+        }(blockingIOThreadPool).map { _ ⇒
+          measure {
+            exchange.sendResponseHeaders(200, 0)
+            exchange.close()
+          }
           chronoManager.generate()
-      }(nonBlockingIOPolling)
-
-
-      chronoManager.stop()
+        }(nonBlockingIOPolling)
+      }
       ()
     }
   )
 
-  private def blockIO(): Unit = {
-    Thread.sleep(100)
+  private def blockIO(): Unit = Thread.sleep(100)
+
+  private def measure[T](f: => T): T = {
+    chronoManager.start()
+    val result = f
+    chronoManager.stop()
+    result
   }
 
   server.start()
   log.info("server is started, main thread will stop")
+
 }
