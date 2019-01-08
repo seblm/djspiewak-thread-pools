97314b0608956f0f5d4015651f8d967893ca5bb8
Sébastian Le Merdy <sebastian.lemerdy@compte-nickel.fr>
2019-01-08 06:12:14 +0100

    Give more time to generate report

--- src/scripts/test.sh
+++ src/scripts/test.sh
@@ -3,4 +3,4 @@
               curl --silent localhost:8080?1 &
 sleep 0.01 && curl --silent localhost:8080?2 &
 sleep 0.02 && curl --silent localhost:8080?3 &
-sleep 1    && curl --silent localhost:8080/generate
+sleep 2    && curl --silent localhost:8080/generate
